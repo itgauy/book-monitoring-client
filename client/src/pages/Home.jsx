30c729d78f3a012f6a685b6edabc1bd6e0d4ccde
itgauy <torreslian@duck.com>
2025-05-04 22:32:46 +0800

    Test

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-red-500'>School Library Books Monitoring</div>
+    <>
+      <div className='text-red-500'>School Library Books Monitoring</div>
+      TEst
+    </>
   )
 }
 
