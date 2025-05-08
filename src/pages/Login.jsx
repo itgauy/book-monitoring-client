5d78fc20821fac267169c81453e96c38b568b9e6
itgauy <torreslian@duck.com>
2025-05-09 03:32:53 +0800

    Update Login component: change password input type to 'password' for improved security

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -65,7 +65,7 @@ const Login = () => {
           <fieldset className='flex flex-col gap-1'>
             <label htmlFor="password">Password</label>
             <InputText
-              type="text"
+              type="password"
               placeholder="Your password"
               id="password"
               name="password"
